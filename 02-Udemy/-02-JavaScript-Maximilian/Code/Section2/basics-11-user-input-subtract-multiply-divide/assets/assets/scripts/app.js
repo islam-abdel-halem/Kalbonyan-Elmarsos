5e15fd21d556abc85753d2768df45e45eaa7c9ff
ESLAM <102685868+ESLAM80@users.noreply.github.com>
2023-02-05 04:06:27 +0200

    fixe errer in code

---
+++ 02-Udemy/-02-JavaScript-Maximilian/Code/Section2/basics-11-user-input-subtract-multiply-divide/assets/assets/scripts/app.js
@@ -0,0 +1,44 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function getUserNumberInput(){
+    return parseInt(userInput.value);
+}
+
+function createAndWriteOutput(operator, resultBeforecalc, calcNumber) {
+    const calcDescription = `${resultBeforecalc} ${operator} ${calcNumber}`
+    outputResult(currentResult, calcDescription);
+}
+
+function add() {
+    const enteredNumber = getUserNumberInput();
+    const initialRsult = currentResult;
+    currentResult = currentResult + enteredNumber;
+    createAndWriteOutput('+', initialRsult, enteredNumber);
+}
+
+function subtract() {
+    const enteredNumber = getUserNumberInput();
+    const initialRsult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    createAndWriteOutput('-', initialRsult, enteredNumber);
+}
+
+function multiply() {
+    const enteredNumber = getUserNumberInput();
+    const initialRsult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createAndWriteOutput('*', initialRsult, enteredNumber);
+}
+
+function divide() {
+    const enteredNumber = getUserNumberInput();
+    const initialRsult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createAndWriteOutput('/', initialRsult, enteredNumber);
+}
+
+addBtn.addEventListener('click',add);
+subtractBtn.addEventListener('click',subtract);
+multiplyBtn.addEventListener('click',multiply);
+divideBtn.addEventListener('click',divide);
