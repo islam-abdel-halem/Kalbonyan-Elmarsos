cfd481c913280136bd12f319ed3a49ad42b7cda0
ESLAM <102685868+ESLAM80@users.noreply.github.com>
2023-02-17 03:50:50 +0200

    section 2 complete

---
+++ 02-Udemy/-02-JavaScript-Maximilian/Code/Section2/basics-13-basic-object/assets/assets/scripts/app.js
@@ -0,0 +1,65 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
+
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+
+function createAndWriteOutput(operator, resultBeforecalc, calcNumber) {
+  const calcDescription = `${resultBeforecalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
+}
+
+function writeToLog(
+  operationIdentifier,
+  prevRedult,
+  operstionNumber,
+  newResult
+) {
+  const logEntry = {
+    operation: operationIdentifier,
+    prevResult: prevRedult,
+    number: operstionNumber,
+    Result: newResult,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
+
+function add() {
+  const enteredNumber = getUserNumberInput();
+  const initialRsult = currentResult;
+  currentResult = currentResult + enteredNumber;
+  createAndWriteOutput("+", initialRsult, enteredNumber);
+  writeToLog("ADD", initialRsult, enteredNumber, currentResult);
+}
+
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const initialRsult = currentResult;
+  currentResult = currentResult - enteredNumber;
+  createAndWriteOutput("-", initialRsult, enteredNumber);
+  writeToLog("SUBTRACT", initialRsult, enteredNumber, currentResult);
+}
+
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const initialRsult = currentResult;
+  currentResult = currentResult * enteredNumber;
+  createAndWriteOutput("*", initialRsult, enteredNumber);
+  writeToLog("MULTIPLY", initialRsult, enteredNumber, currentResult);
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const initialRsult = currentResult;
+  currentResult = currentResult / enteredNumber;
+  createAndWriteOutput("/", initialRsult, enteredNumber);
+  writeToLog("DIVIDE", initialRsult, enteredNumber, currentResult);
+}
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
