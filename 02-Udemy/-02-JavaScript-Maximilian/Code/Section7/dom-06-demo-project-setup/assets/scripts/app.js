3a43c644b0295bf32740dbf933b18d6fe242bded
ESLAM <102685868+ESLAM80@users.noreply.github.com>
2023-03-31 14:44:10 +0200

    Add Project Setup

---
+++ 02-Udemy/-02-JavaScript-Maximilian/Code/Section7/dom-06-demo-project-setup/assets/scripts/app.js
@@ -0,0 +1,6 @@
+const addMovieModal = document.getElementById('add-modal');
+// const addMovieModal = document.querySelector('#add-modal');
+// const addMovieModal = document.body.children[1];
+const startAddMovieModal = document.querySelector('header buttom');
+// const startAddMovieModal = document.querySelector('header').lastElementChild;
+console.log(addMovieModal);
